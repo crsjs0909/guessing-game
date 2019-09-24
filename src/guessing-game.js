2a44f10af7fad9a2fdb577b08f2d8f54cf5427d0
oleg kin <noklige@gmail.com>
2019-09-25 02:41:52 +0300

    feat: class implemented

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
+        this.last = null;
     }
 
     guess() {
-
+        this.last = Math.ceil((this.max + this.min) / 2);
+        return this.last;
     }
 
     lower() {
-
+        this.max = this.max === this.last ? this.last - 1 : this.last;
     }
 
     greater() {
-
+        this.min = this.last;
     }
 }
 
